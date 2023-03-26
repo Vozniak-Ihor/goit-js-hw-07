d5016e5bf5e9b83913e3ea50af7b1b77476db560
Vozniak-Ihor <118538389+Vozniak-Ihor@users.noreply.github.com>
2023-03-26 14:00:39 +0300

    створено галерею

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+function createFotoCads(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}"> 
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li> `;
+    })
+    .join('');
+}
+
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkup = createFotoCads(galleryItems);
+galleryEl.insertAdjacentHTML("beforebegin", galleryMarkup);
+
+
