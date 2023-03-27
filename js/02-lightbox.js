021db88e0af5ecd85360a970c8f8b185d13372dc
Vozniak-Ihor <118538389+Vozniak-Ihor@users.noreply.github.com>
2023-03-27 16:01:18 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,32 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
-
 function createFotoCads(galleryItems) {
-    return galleryItems
-      .map(({ preview, original, description }) => {
-        return ` <li class="gallery__item">
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return ` <li class="gallery__item">
    <a class="gallery__link" href="${original}">
       <img class="gallery__image" src="${preview}" alt="${description}" />
    </a>
-</li>`
-      })
-      .join("");
-  }
-  
-  const galleryEl = document.querySelector(".gallery");
-  const galleryMarkup = createFotoCads(galleryItems);
-  galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+</li>`;
+    })
+    .join("");
+}
 
-  let gallery = new SimpleLightbox('.gallery a', {captionsData: "alt",  captionPosition:"bottom", captionDelay: 250,  scrollZoom: false});
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkup = createFotoCads(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
 
-gallery.on('shown.simplelightbox', function (event) {
-  event.preventDefault();
-  console.log("Everything works fine")
+const gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  doubleTapZoom: 1.7,
+  alertErrorMessage: "Image not found, next image will be loaded",
 });
 
-gallery.on('error.simplelightbox', function (event) {
-	console.log(event, "Something wrong"); 
+gallery.on("shown.simplelightbox", function (event) {
+  event.preventDefault();
+  console.log("Everything works fine");
+});
+
+gallery.on("error.simplelightbox", function (event) {
+  console.log(event, "Something wrong");
 });
