ce29d5a3c6415bfadc84a071ac760e0335e8f2a1
Vozniak-Ihor <118538389+Vozniak-Ihor@users.noreply.github.com>
2023-04-04 23:09:14 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,22 +1,23 @@
-import { galleryItems } from "./gallery-items.js";
-// Change code below this line
+import galleryItems from "./gallery-items.js";
 
 console.log(galleryItems);
 
-function createFotoCads(galleryItems) {
-  return galleryItems
+function createPhotoCards(items) {
+  return items
     .map(({ preview, original, description }) => {
-      return ` <li class="gallery__item">
-   <a class="gallery__link" href="${original}">
-      <img class="gallery__image" src="${preview}" alt="${description}" />
-   </a>
-</li>`;
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+        </li>
+      `;
     })
     .join("");
 }
 
 const galleryEl = document.querySelector(".gallery");
-const galleryMarkup = createFotoCads(galleryItems);
+const galleryMarkup = createPhotoCards(galleryItems);
 galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
 
 const gallery = new SimpleLightbox(".gallery a", {
@@ -27,11 +28,11 @@ const gallery = new SimpleLightbox(".gallery a", {
   alertErrorMessage: "Image not found, next image will be loaded",
 });
 
-gallery.on("shown.simplelightbox", function (event) {
+gallery.on("shown.simplelightbox", function handleGalleryShown(event) {
   event.preventDefault();
   console.log("Everything works fine");
 });
 
-gallery.on("error.simplelightbox", function (event) {
+gallery.on("error.simplelightbox", function handleGalleryError(event) {
   console.log(event, "Something wrong");
 });
